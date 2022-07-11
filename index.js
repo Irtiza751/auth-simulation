2cbfdfa0382dfd2a5cf0143b6e386e4f258eaa4e
Irtiza751 <muhammad.irtiza751@gmail.com>
2022-07-11 21:29:04 +0500

    Add enviroment port

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ app.get('/friends', (req, res) => {
   res.json(friends)
 });
 
-app.listen(3000, () => console.log('server started on port: 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('server started on port: 3000'));
